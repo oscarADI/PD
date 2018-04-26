671217739bbdc1d033786349c68e21e3b54f06a9
oscarADI <horn1720@gmail.com>
2018-04-26 18:46:47 +0800

    ...

--- PA2/src/main.cpp
+++ PA2/src/main.cpp
@@ -5,17 +5,13 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    Floorplan test(atof(argv[1]));
+    Floorplan test;
+    test.seta(argv[1]);
     test.parser_block(argv[2]);
-    //test.debug();
     test.parser_net(argv[3]);
     // test.debug();
     test.init_tree();
-    
     test.packing();
-    // test.debug();
     test.compute_norm();
     test.SA();
-    // test.debug();
-
 }
